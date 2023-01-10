3e156ea491d55a1a38810b931d836c6e9eb78f5d
sattala <sattala.kushal@gmail.com>
2023-01-09 19:49:26 -0600

    changed all

--- catalogue.sh
+++ catalogue.sh
@@ -8,6 +8,7 @@ if [ $? -eq 0 ]; then
   echo SUCCESS
 else
   echo FAILURE
+exit
 fi
 
 echo -e "\e[35m Install NodeJS repos\e[0m"
@@ -16,6 +17,7 @@ if [ $? -eq 0 ]; then
   echo SUCCESS
 else
   echo FAILURE
+exit
 fi
 
 echo -e "\e[35m Add Application User\e[0m"
@@ -24,16 +26,18 @@ if [ $? -eq 0 ]; then
   echo SUCCESS
 else
   echo FAILURE
+exit
 fi
 
 mkdir -p /app &>>${LOG}
 
 echo -e "\e[35m Downloading App content\e[0m"
-curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${LOG} 
+curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${LOG}
 if [ $? -eq 0 ]; then
   echo SUCCESS
 else
   echo FAILURE
+exit
 fi
 
 echo -e "\e[35m Cleaning Old Content\e[0m"
@@ -42,15 +46,17 @@ if [ $? -eq 0 ]; then
   echo SUCCESS
 else
   echo FAILURE
+exit
 fi
 
 echo -e "\e[35m Extracting App Content\e[0m"
-cd /app 
+cd /app
 unzip /tmp/catalogue.zip &>>${LOG}
 if [ $? -eq 0 ]; then
   echo SUCCESS
 else
   echo FAILURE
+exit
 fi
 
 echo -e "\e[35m Installing NodeJS Dependencies\e[0m"
@@ -60,6 +66,7 @@ if [ $? -eq 0 ]; then
   echo SUCCESS
 else
   echo FAILURE
+exit
 fi
 
 echo -e "\e[35m Configuring Catalogue Service File\e[0m"
@@ -68,6 +75,7 @@ if [ $? -eq 0 ]; then
   echo SUCCESS
 else
   echo FAILURE
+exit
 fi
 
 echo -e "\e[35m Reload systemD\e[0m"
@@ -76,6 +84,7 @@ if [ $? -eq 0 ]; then
   echo SUCCESS
 else
   echo FAILURE
+exit
 fi
 
 echo -e "\e[35m Enable Catalogue service\e[0m"
@@ -84,6 +93,7 @@ if [ $? -eq 0 ]; then
   echo SUCCESS
 else
   echo FAILURE
+exit
 fi
 
 echo -e "\e[35m Start Catalogue service\e[0m"
@@ -92,6 +102,7 @@ if [ $? -eq 0 ]; then
   echo SUCCESS
 else
   echo FAILURE
+exit
 fi
 
 echo -e "\e[35m Configuring Mongo Repo\e[0m"
@@ -100,6 +111,7 @@ if [ $? -eq 0 ]; then
   echo SUCCESS
 else
   echo FAILURE
+exit
 fi
 
 echo -e "\e[35m Install Mongo Client\e[0m"
@@ -108,6 +120,7 @@ if [ $? -eq 0 ]; then
   echo SUCCESS
 else
   echo FAILURE
+exit
 fi
 
 echo -e "\e[35m Load Schema\e{0m"
@@ -116,7 +129,9 @@ if [ $? -eq 0 ]; then
   echo SUCCESS
 else
   echo FAILURE
+exit
 fi
+
 
 
 
