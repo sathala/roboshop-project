f572e73fb34b13ddf331227c60e21e773c50e56b
sattala <sattala.kushal@gmail.com>
2023-01-10 16:16:47 -0600

    new change

--- redis.sh
+++ redis.sh
@@ -1,19 +1,19 @@
-source common .sh
+source common.sh
 
 print_head "Setup Redis Repo"
-yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>${LOG} 
+yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>${LOG}
 status_check
 
-print_head "Enable Redis 602 dbf Module"
-dnf module enable redis:remi-602 -y &>>${LOG}
+print_head "Enable Redis 6.2 dnf Module"
+dnf module enable redis:remi-6.2 -y &>>${LOG}
 status_check
 
 print_head "Install Redis"
-yum install Redis -y &>>${LOG} 
+yum install redis -y  &>>${LOG}
 status_check
 
 print_head "Update Redis Listen Address"
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf &>>${LOG} 
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf &>>${LOG}
 status_check
 
 
