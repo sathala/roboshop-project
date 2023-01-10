dcd2e7392532a097dc9737706646b9b2936b8f7c
sattala <sattala.kushal@gmail.com>
2023-01-10 17:23:49 -0600

    changes

--- common.sh
+++ common.sh
@@ -2,13 +2,13 @@ script_location=$(pwd)
 LOG=/tmp/roboshop.log
 
 status_check() {
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
+  if [ $? -eq 0 ]; then
+   echo -e "\e[1;32mSUCCESS\e[0m"
 else
-  echo -e "\e[31mFAILUER\e[0m"
-  echo "Refer log files for more information, LOG - ${LOG}"
-  exit
-fi 
+   echo -e "\e[1;31mFAILUER\e[0m"
+   echo "Refer log file for more information, LOG - ${LOG}"
+   exit
+  fi 
 }
 
 print_head() {
