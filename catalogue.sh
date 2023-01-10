20d5cfa67927b7e8afc5e121da68871a6280e8b4
sattala <sattala.kushal@gmail.com>
2023-01-09 20:37:49 -0600

    code change

--- catalogue.sh
+++ catalogue.sh
@@ -1,16 +1,4 @@
-script_location=$(pwd)
-LOG=/tmp/roboshop.log
-
-status_check() {
-  if [ $? -eq 0 ]; then
-    echo -e "\e[32mSUCCESS\e[0m"
-   else
-   echo -e "\e[31mFAILUER\e[0m"
-  echo "Refer log files for more information, LOG - ${LOG}"
-exit
-fi 
-}
-
+source common.sh
 
 echo -e "\e[35m Configuring NodeJS repos\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG}
