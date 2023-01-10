b1524723941c72f1f8f4cabc93cb90223b352dd8
sattala <sattala.kushal@gmail.com>
2023-01-10 17:25:11 -0600

    cdc

--- common.sh
+++ common.sh
@@ -3,12 +3,12 @@ LOG=/tmp/roboshop.log
 
 status_check() {
   if [ $? -eq 0 ]; then
-   echo -e "\e[1;32mSUCCESS\e[0m"
-else
-   echo -e "\e[1;31mFAILUER\e[0m"
-   echo "Refer log file for more information, LOG - ${LOG}"
-   exit
-  fi 
+    echo -e "\e[1;32mSUCCESS\e[0m"
+  else
+    echo -e "\e[1;31mFAILURE\e[0m"
+    echo "Refer Log file for more information, LOG - ${LOG}"
+    exit 1
+  fi
 }
 
 print_head() {
