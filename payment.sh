1c3aa2cb724480514eb6ca399cbcbc4d83553631
sattala <sattala.kushal@gmail.com>
2023-01-17 16:36:21 -0600

    error

--- payment.sh
+++ payment.sh
@@ -1,7 +1,7 @@
 source common.sh
 
 component=payment
-schema_load=fales
+schema_load=false
 
 if [ -z "${roboshop_rabbitmq_password}" ]; then
   echo "Variable roboshop_rabbitmq_password is missing"
